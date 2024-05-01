2d3193b4c655acb7941068b08d515d298db201da
syeda <sbtshakeel61@gmail.com>
2024-05-01 18:14:34 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+//ASYNC TS
+console.log ("SALAM") //first print
+
+setTimeout(() =>{
+    console.log("set time out run after 2 sec") //it will print last because we set a time out for maybe 2 second.
+},
+2000)//set time
+
+console.log ("HELLO") //second print
